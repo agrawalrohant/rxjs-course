c334bbf8f8ead63510a86285ce342eed3a8c955a
agrawalrohant <agrawalrohant@gmail.com>
2020-05-05 22:49:26 +0530

    understanding strams

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,6 +10,27 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    // Multivalue Stream - value emmited with interaction (never complete)
+    document.addEventListener('click', evt => {
+      console.log(evt);
+    });
+
+    // Multivalue Stream - value emmited continious (never complete)
+    let counter = 0;
+    setInterval(
+      () => {
+        console.log(counter);
+        counter++;
+      }, 1000
+    );
+
+    // SIngle value Stream - Value emmited only once
+    setTimeout(() => {
+      console.log(`finished...`);
+    }, 3000
+    );
+
   }
 
 }
