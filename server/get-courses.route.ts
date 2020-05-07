8eb8897a0a3c695c7fa769dce5b1f143b4a260a4
agrawalrohant <agrawalrohant@gmail.com>
2020-05-08 00:32:16 +0530

    recovery error handling strategy

--- server/get-courses.route.ts
+++ server/get-courses.route.ts
@@ -1,7 +1,7 @@
 
 
-import {Request, Response} from 'express';
-import {COURSES} from "./db-data";
+import { Request, Response } from 'express';
+import { COURSES } from "./db-data";
 
 
 
@@ -12,18 +12,18 @@ export function getAllCourses(req: Request, res: Response) {
 
     if (error) {
         console.log("ERROR loading courses!");
-        res.status(500).json({message: 'random error occurred.'});
     }
     else {
-    */
+        */
 
     setTimeout(() => {
 
-        res.status(200).json({payload:Object.values(COURSES)});
+        res.status(500).json({ message: 'random error occurred.' });
+        //res.status(200).json({payload:Object.values(COURSES)});
 
     }, 200);
 
-   // }
+    // }
 }
 
 
@@ -31,7 +31,7 @@ export function getCourseById(req: Request, res: Response) {
 
     const courseId = req.params["id"];
 
-    const courses:any = Object.values(COURSES);
+    const courses: any = Object.values(COURSES);
 
     const course = courses.find(course => course.id == courseId);
 
