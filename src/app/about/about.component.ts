b324379d89e73b41dfcb6c71fb813440fb4904f0
agrawalrohant <agrawalrohant@gmail.com>
2020-05-08 17:34:12 +0530

    Behavious Subject

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject } from 'rxjs';
+import { concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, BehaviorSubject } from 'rxjs';
 import { delayWhen, filter, map, take, timeout } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
 
@@ -13,16 +13,24 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
-        const subject = new Subject();
+        const subject = new BehaviorSubject(0);
 
         const series$ = subject.asObservable();
 
-        series$.subscribe(console.log);
+        series$.subscribe(val => console.log("Early sub : " + val));
 
         subject.next(1);
         subject.next(2);
         subject.next(3);
-        subject.complete();
+        // If the Subject is completed, late subscriber will not get anything.
+        //subject.complete();
+
+        setTimeout(() => {
+            // late subscriber will get the lat value emmited. In this case '3'
+            series$.subscribe(val => console.log("late sub : " + val));
+            subject.next(4);
+
+        }, 3000);
     }
 
 
